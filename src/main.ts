8165a6132d17d1c6793dc7f893147e58218e1958
edgardoll4 <edgardoll4@gmail.com>
2022-10-07 22:28:43 -0400

    Prueba documentación

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,6 +17,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentación de la API test nestjs shop')
+    .setDescription('The API description')
+    .setVersion('1.0') // .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+
+
   await app.listen(process.env.SERV_PORT);
   logger.log(`App ejecutandoce en el puerto: ${process.env.SERV_PORT}`);
   logger.log(`Más informcaión visite. ${process.env.SERV_HOST}:${process.env.SERV_PORT}/api`);
